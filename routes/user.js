60ad64b1311165174d2c81553a55a519404152b5
Роман Бубнов <yarepka01@gmail.com>
2020-05-01 22:05:03 +0300

    Simplified code in profile route

--- routes/user.js
+++ routes/user.js
@@ -30,12 +30,7 @@ router.get("/profile", isLoggedIn, (req, res, next) => {
       order.items = cart.generateArray();
     });
 
-    let ordersReversed = [];
-    for (let i = orders.length - 1, j = 0; i >= 0; i--, j++) {
-      ordersReversed[j] = orders[i];
-    }
-
-    res.render("user/profile", { orders: ordersReversed });
+    res.render("user/profile", { orders: orders.reverse() });
   });
 });
 
